8aa918d5fc2c4ddd05d96e3df0cd1b6969671bab
Diego <83728849+c-diego@users.noreply.github.com>
2022-06-07 00:23:49 -0300

    Update Barbearia.java

--- src/dorminhoco/Barbearia.java
+++ src/dorminhoco/Barbearia.java
@@ -1,10 +1,32 @@
 package dorminhoco;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  *
  * @author suporte
  */
 public class Barbearia {
-    
-    
+
+    public static void main(String args[]) throws InterruptedException {
+        SalaEspera fila = new SalaEspera(5);
+        Lock lock = new ReentrantLock();
+        Condition condiiton = lock.newCondition();
+
+        Barbeiro barbeiro = new Barbeiro(fila, lock, condiiton);
+        Recepcionista recepcionista = new Recepcionista(fila, lock, condiiton);
+
+        Thread t1 = new Thread(barbeiro);
+        Thread t2 = new Thread(recepcionista);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+    }
+
 }
