2d187d61ce5876678025a7222dde2272ab248d2e
Diego <83728849+c-diego@users.noreply.github.com>
2022-06-07 00:22:03 -0300

    Atualizando SalaEspera.java

--- src/dorminhoco/SalaEspera.java
+++ src/dorminhoco/SalaEspera.java
@@ -1,28 +1,44 @@
 package dorminhoco;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 /**
  *
  * @author suporte
  */
 public class SalaEspera {
-    
-    private ConcurrentLinkedQueue<Cliente> fila;
-    
-    public SalaEspera() {
-        fila = new ConcurrentLinkedQueue<>();
+
+    private final ArrayBlockingQueue<Cliente> fila;
+
+    public SalaEspera(int MAX) {
+        fila = new ArrayBlockingQueue<>(MAX);
     }
-    
-    public boolean add(Cliente cliente) {
-        return fila.add(cliente);
+
+    public boolean isFull() {
+        return fila.remainingCapacity() <= 0;
     }
-    
-    public Cliente get() {
-        return fila.remove();
-    }
-    
+
     public boolean isEmpty() {
         return fila.isEmpty();
+    }
+
+    public boolean put(Cliente cliente) {
+        if (!isFull())
+            try {
+                fila.put(cliente);
+                return true;
+        } catch (InterruptedException ex) {}
+        return false;
+    }
+
+    public Cliente remove() {
+        try {
+            return fila.take();
+        } catch (InterruptedException ex) {}
+        return null;
+    }
+
+    public int getSize() {
+        return fila.size();
     }
 }
